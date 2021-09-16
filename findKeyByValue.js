5e441fa1c7ceee7a29252440718d90d84af99cdb
Nick <nsagias@gmail.com>
2021-09-16 01:04:33 -0400

    Intialise findKeyByValue with test cases

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, string) {
+  let results = "";
+  
+  return results;
+};
+
+
+// test cases
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // => PASS
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // 
