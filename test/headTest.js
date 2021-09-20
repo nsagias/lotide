e362f09ea3965ecec079d68256c39a81820c137c
Nick <nsagias@gmail.com>
2021-09-20 18:46:43 -0400

    corrected compare, passing

--- test/headTest.js
+++ test/headTest.js
@@ -2,6 +2,6 @@ const head =  require('../head.js');
 const assertEqual = require('../assertEqual.js');
 
 // tests
-assertEqual(head([]), 5);
+assertEqual(head([]), undefined);
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
