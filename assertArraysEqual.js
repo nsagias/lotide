b76f61fca3426c97d8a96218090f2d963f7649a4
Nick <nsagias@gmail.com>
2021-09-20 15:48:32 -0400

    refactor assertArrayEquals and test, change assertion fnc

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  if (array1[0] !== array2[0]) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1,array2)) {
@@ -22,5 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+module.exports = assertArraysEqual;
